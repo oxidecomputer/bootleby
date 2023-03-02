72f4163063caf9115690faa980cc7dc013e7fc33
Cliff L. Biffle <cliff@oxide.computer>
2023-03-02 12:40:05 -0800

    Use build.rs to check feature settings.
    This will fail if there isn't exactly one target-feature-* feature.

---
+++ build.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let mut target_board: Option<String> = None;
+
+    for (name, _) in std::env::vars() {
+        let prefix = "CARGO_FEATURE_TARGET_BOARD_";
+        if name.starts_with("CARGO_FEATURE_TARGET_BOARD_") {
+            let suffix = name[prefix.len()..].to_string();
+            if let Some(previous) = &target_board {
+                panic!("multiple target board features defined (at least {} and {})",
+                    show_feature(previous), show_feature(&suffix));
+            }
+
+            target_board = Some(suffix);
+        }
+    }
+
+    if target_board.is_none() {
+        panic!("missing target-board-* feature");
+    }
+}
+
+fn show_feature(envvar: &str) -> String {
+    let mut name = "target-board-".to_string();
+    name.push_str(
+        &envvar.to_ascii_lowercase()
+            .replace('_', "-")
+    );
+    name
+}
