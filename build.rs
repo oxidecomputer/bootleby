3e10edc514b47f31a3f97b2d4821ebe4c1d86115
Laura Abbott <labbott@users.noreply.github.com>
2023-11-15 08:28:06 -0800

    Add Image Header to bootleby (#41)
    We want to be able to add a caboose post build to store some meta
    information. This requires an image header that lives right after
    the vector table. The easiest way to add this is with a custom
    linker script.

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+
 fn main() {
     let mut target_board: Option<String> = None;
 
@@ -20,6 +25,43 @@ fn main() {
     if target_board.is_none() {
         panic!("missing target-board-* feature");
     }
+
+    // Could add a feature to make this optional
+    gen_linker_script();
+}
+
+fn gen_linker_script() {
+    // TODO Pull out into another crate?
+    #[repr(C)]
+    struct ImageHeader {
+        magic: u32,
+        total_image_len: u32,
+        _pad: [u32; 16], // previous location of SAU entries
+        version: u32,
+        epoch: u32,
+    }
+
+    println!("cargo:rerun-if-changed=link.x.in");
+    let base = std::fs::read("link.x.in").unwrap();
+
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let mut linker_script = File::create(out.join("link.x")).unwrap();
+
+    writeln!(
+        linker_script,
+        "_HUBRIS_IMAGE_HEADER_ALIGN = {:#x};",
+        std::mem::align_of::<ImageHeader>()
+    )
+    .unwrap();
+    writeln!(
+        linker_script,
+        "_HUBRIS_IMAGE_HEADER_SIZE = {:#x};",
+        std::mem::size_of::<ImageHeader>()
+    )
+    .unwrap();
+
+    linker_script.write_all(&base).unwrap();
+    println!("cargo:rustc-link-search={}", out.display());
 }
 
 fn show_feature(envvar: &str) -> String {
